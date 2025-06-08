0a7c008a0b22a9ec0d70f918ebcef7d5980e7194
ksoinski-cdv <ksoinski@edu.cdv.pl>
2025-06-08 17:49:43 +0200

    initialize Django project

---
+++ smartmailbox/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Device(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=50, unique=True)
+    security_code = models.CharField(max_length=20)
+    battery_level = models.IntegerField(default=100)
+    last_package_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.device_id} (owner: {self.owner.username})"
+
+# Create your models here.
