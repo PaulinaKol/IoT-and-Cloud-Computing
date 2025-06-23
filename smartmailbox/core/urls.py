5092c03cc4fe0075d7fa2a8583efcc6c49c2a461
ksoinski-cdv <ksoinski@edu.cdv.pl>
2025-06-23 12:41:30 +0200

    add "forgot password" option in login view

--- smartmailbox/core/urls.py
+++ smartmailbox/core/urls.py
@@ -11,6 +11,15 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', user_views.CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset/password_reset.html',
+        email_template_name='password_reset/password_reset_email.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset/password_reset_complete.html'), name='password_reset_complete'),
 
     # === Aktywacja i ustawienia konta ===
     path('activate/', user_views.activate_account, name='activate_account'),
