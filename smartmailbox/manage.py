4783425053dea4acb2cf149ad32bba3ec2ecef89
ksoinski-cdv <ksoinski@edu.cdv.pl>
2025-06-27 17:39:17 +0200

    migration attempt 2 start

--- smartmailbox/manage.py
+++ smartmailbox/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartmailbox.settings')
+    settings_module = 'smartmailbox.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'smartmailbox.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- smartmailbox/smartmailbox/wsgi.py
+++ smartmailbox/smartmailbox/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartmailbox.settings')
+settings_module = 'smartmailbox.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'smartmailbox.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
